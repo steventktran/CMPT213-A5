1166219ac2b1b787676b769705203271e90512d6
steventktran <sttran@sfu.ca>
2018-03-29 14:20:13 -0700

    Added CourseData and Application classes, but don't know if we need CourseData

---
+++ src/main/java/ca/cmpt213/as5/model/CourseData.java
@@ -0,0 +1,11 @@
+package ca.cmpt213.as5.model;
+
+public class CourseData {
+    private int semNumber;
+    private String subject;
+    private String courseNum;
+    private String location;
+    private int enrollmentCap;
+    private int enrollmentTotal;
+    private String enrollment;
+}
