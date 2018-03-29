aa6d2dd84a63e553a333959129d69d9264f1e935
steventktran <sttran@sfu.ca>
2018-03-29 14:50:45 -0700

    csv parser added but unsure if it works yet

--- src/main/java/ca/cmpt213/as5/model/CSVParser.java
+++ src/main/java/ca/cmpt213/as5/model/CSVParser.java
@@ -1,4 +1,28 @@
 package ca.cmpt213.as5.model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class CSVParser {
+    private List<CourseData> courseList = new ArrayList<>();
+    private File file;
+
+    public CSVParser(File file) {
+        this.file = file;
+    }
+
+    public void parseFile() {
+        try {
+            Scanner read = new Scanner(file);
+            while (read.hasNextLine()) {
+                String[] fields = read.nextLine().split(",");
+                courseList.add(new CourseData(fields));
+            }
+        } catch (FileNotFoundException e){
+
+        }
+    }
 }
--- src/main/java/ca/cmpt213/as5/model/CourseData.java
+++ src/main/java/ca/cmpt213/as5/model/CourseData.java
@@ -1,5 +1,7 @@
 package ca.cmpt213.as5.model;
 
+import static java.lang.Integer.*;
+
 public class CourseData {
     private int semNumber;
     private String subject;
@@ -7,5 +9,75 @@ public class CourseData {
     private String location;
     private int enrollmentCap;
     private int enrollmentTotal;
-    private String enrollment;
+    private String component;
+
+    public CourseData(String[] fields) {
+        semNumber = parseInt(fields[0]);
+        subject = fields[1];
+        courseNum = fields[2];
+        location = fields[3];
+        enrollmentCap = parseInt(fields[4]);
+        enrollmentTotal = parseInt(fields[5]);
+        component = fields[6];
+    }
+
+    public CourseData() {
+
+    }
+
+    public int getSemNumber() {
+        return semNumber;
+    }
+
+    public void setSemNumber(int semNumber) {
+        this.semNumber = semNumber;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getCourseNum() {
+        return courseNum;
+    }
+
+    public void setCourseNum(String courseNum) {
+        this.courseNum = courseNum;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getEnrollmentCap() {
+        return enrollmentCap;
+    }
+
+    public void setEnrollmentCap(int enrollmentCap) {
+        this.enrollmentCap = enrollmentCap;
+    }
+
+    public int getEnrollmentTotal() {
+        return enrollmentTotal;
+    }
+
+    public void setEnrollmentTotal(int enrollmentTotal) {
+        this.enrollmentTotal = enrollmentTotal;
+    }
+
+    public String getComponent() {
+        return component;
+    }
+
+    public void setComponent(String component) {
+        this.component = component;
+    }
 }
