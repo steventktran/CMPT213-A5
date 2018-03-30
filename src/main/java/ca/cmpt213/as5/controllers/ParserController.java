5d3cec28e2bdc58d3dcfe6c2c252e081c795f083
Andy W <wuandyw@sfu.ca>
2018-03-29 17:30:41 -0700

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/ca/cmpt213/as5/controllers/ParserController.java
+++ src/main/java/ca/cmpt213/as5/controllers/ParserController.java
@@ -1,4 +1,24 @@
 package ca.cmpt213.as5.controllers;
 
+import ca.cmpt213.as5.model.CSVParser;
+import ca.cmpt213.as5.model.CourseData;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ParserController {
+    private static final String FILE_PATH = "data/course_data_2018.csv";
+    private CSVParser theParser;
+
+    //Get Mapping
+    @GetMapping("/dump-model")
+    public List<CourseData> getDumpModel() {
+        List<CourseData> data = null;
+        return data;
+    }
+
+
 }
